9466e118482786bfa7f93138081f0e8195301290
dennis.rocha <dennis.rocha@neoway.com.br>
2021-09-29 13:40:43 -0300

    Functions of the students

---
+++ course.py
@@ -0,0 +1,50 @@
+
+import re
+from unicodedata import name
+
+
+def validateCourse(data):
+    """-------NAME FILTER-------"""
+    try:
+        data["name"] = data["name"].strip().upper()
+    
+    except:
+        pass
+    
+
+    """-------DESCRIPTION FILTER-------"""
+    try:
+        data["description"] = data["description"].strip()
+    
+    except:
+        if data["description"] != str:
+            return {
+                "status_code":400,
+                "message":"The description needs text"
+            },False
+    
+    else:
+        if len(data["description"]) < 10:
+            return {
+                "status_code":422,
+                "message":"The description is very small, need description > 10"    
+            }, False
+            
+    """-------DURATION FILTER-------"""
+    try:
+        duration="".join(re.findall("[0-9]",data['duration']))
+    
+    except:
+        pass
+    
+    else:
+        if not duration:
+            return {
+                "status_code":400,
+                "message":"The key 'duration' needs int numbers"
+            },False
+        
+        else:
+            data["duration"] = int(duration)
+            
+    return data,True
